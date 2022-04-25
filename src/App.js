94c07e4762d3463acd4c160d10f4dadfb0d757a3
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-04-25 09:39:38 -0300

    realizo states, declaro variables apikey y getapi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //STATE
+  const [chainId, setchainId] = useState("")
+  const [collectionAdress, setcollectionAdress] = useState("")
+
+  //UseEffect
+  useEffect(() => {
+    getAp();
+    getApi();
+  }, []);
+
+  //apikey
+
+  const apiKey = "ckey_99eb99abfb544235823be4803af";
+
+  //API
+
+const apiCovalent = `https://api.covalenthq.com/v1/${chainId}/nft_market/collection/${collectionAdress}/?&key=${apiKey}`
+
+const getApi = async ()=>{
+
+  try {
+
+    const res = await fetch(apiCovalent)
+    console.log(res)
+
+    
+  } catch (error) {
+    console.log(error)
+    
+  }
+}
+
+//handle submit
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  
+
+};
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <label>Chain Id</label>
+        <input type="text" placeholder='Ingrese el Chain ID' onChange={(e)=>setchainId(e.target.value)}/>
+        <label>Collection Adress</label>
+        <input type="text" placeholder='Ingrese el Collection Adress' onChange={(e)=>setcollectionAdress(e.target.value)}/>
+        <button></button>
+
+
+      </form>
       
     </div>
   );
